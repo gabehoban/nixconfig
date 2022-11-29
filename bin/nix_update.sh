b361d15b22da67486ba26f1d1395dbd6bb6def69
gabehoban <hello@gabehoban.com>
2022-11-28 22:46:54 -0500

    Working copy.

--- bin/nix_update.sh
+++ bin/nix_update.sh
@@ -2,6 +2,6 @@
 
 cd ~/.config/nixconfig
 
-darwin-rebuild switch --flake .
+darwin-rebuild switch --flake . \
 && sudo nix-store --verify --check-contents --repair      \
 && osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"~/.config/nixconfig/pictures/rocket-space-minimal.jpeg\" as POSIX file"
